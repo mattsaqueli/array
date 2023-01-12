1a52242c99897b623236415f616fdf778873fc2c
Matt Saqueli <msaqueli22@turing.edu>
2023-01-11 18:06:01 -0600

    Add info on index positions

--- arrays.js
+++ arrays.js
@@ -11,3 +11,9 @@ console.log(eggPrices);
 // The push method will add on another false at the end of the array.
 paidFee.push(false);
 console.log(paidFee);
+
+
+// Index positions are positions that the elements in the array are in.
+// The firs element starts the count at 0 and increases by 1.
+// Example: "Matt" is in index position 0.
+// Example: "15" is in index position 1.
