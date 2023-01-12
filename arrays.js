e206a9aacf097dba5523ad643b65d0720bdda9a3
Matt Saqueli <msaqueli22@turing.edu>
2023-01-11 18:02:25 -0600

    Add array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,13 @@
 var friends = ["Matt", "Sarah", "Devon", "Justin"];
 var eggPrices = [10, 15, 20, 25];
 var paidFee = [true, true, false, false];
+
+// The pop method will remove the last name in the array, Justin.
+friends.pop();
+console.log(friends);
+// The shift method will remove the first element from the array.
+eggPrices.shift(1);
+console.log(eggPrices);
+// The push method will add on another false at the end of the array.
+paidFee.push(false);
+console.log(paidFee);
